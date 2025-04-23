0bba56a86e34ecf8465fb658e638e8a99829c9a6
Tyler Moroz <moroztyler@gmail.com>
2025-04-22 21:26:27 -0600

    Adjustments to font sizing and capitalize all pokemon names.

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -22,7 +22,7 @@ function PokemonCard({ name, onClick }) {
 
   return (
     <div className="pokemon-card" onClick={onClick}>
-      <h1>{pokemon.name}</h1>
+      <h1>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h1>
       <img src={pokemon.sprites.front_default} alt={pokemon.name} />
     </div>
   );
